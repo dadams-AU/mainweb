a5ff562092cbb50d691e200810b22607c00657da
dadams-AU <dpadams@fullerton.edu>
2023-06-05 18:24:31 -0700

    nav bar, dark mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,32 +1,34 @@
-// JavaScript Document
-const checkbox = document.getElementById('themeSwitch');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const checkbox = document.getElementById('themeSwitch');
+  // Check for saved 'darkMode' in localStorage
+  let darkMode = localStorage.getItem('darkMode'); 
 
-// Check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode'); 
+  const enableDarkMode = () => {
+    // Add the class to the body
+    document.body.classList.add('dark');
+    // Update darkMode in localStorage
+    localStorage.setItem('darkMode', 'enabled');
+  }
 
-const enableDarkMode = () => {
-  // Add the class to the body
-  document.body.classList.add('dark');
-  // Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
-}
+  const disableDarkMode = () => {
+    // Remove the class from the body
+    document.body.classList.remove('dark');
+    // Update darkMode in localStorage 
+    localStorage.setItem('darkMode', null);
+  }
 
-const disableDarkMode = () => {
-  // Remove the class from the body
-  document.body.classList.remove('dark');
-  // Update darkMode in localStorage 
-  localStorage.setItem('darkMode', null);
-}
-
-if (darkMode === 'enabled') {
-  enableDarkMode();
-}
-
-checkbox.addEventListener('change', (event) => {
-  darkMode = localStorage.getItem('darkMode'); 
-  if (darkMode !== 'enabled') {
+  if (darkMode === 'enabled') {
     enableDarkMode();
-  } else {  
-    disableDarkMode(); 
+  }
+
+  if (checkbox) {
+    checkbox.addEventListener('change', () => {
+      darkMode = localStorage.getItem('darkMode'); 
+      if (darkMode !== 'enabled') {
+        enableDarkMode();
+      } else {  
+        disableDarkMode(); 
+      }
+    });
   }
 });
